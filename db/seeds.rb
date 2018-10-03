64bfdc3f2c8557a28b68e9049bbb211395dc9883
prakash2kx <prakashror15@gmail.com>
2018-10-03 08:41:44 +0530

    creating user by seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(email: "prakash@mail.com", password: "12345", user_type: 0)
+User.create(email: "prakash1@mail.com", password: "12345", user_type: 0)
+User.create(email: "doctor@mail.com", password: "12345", user_type: 1)
